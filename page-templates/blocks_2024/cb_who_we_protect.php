a125e579377a7762f8e3cb91adf32444471e3c30
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-06-04 12:24:07 +0100

    Update cb_who_we_protect.php

--- page-templates/blocks_2024/cb_who_we_protect.php
+++ page-templates/blocks_2024/cb_who_we_protect.php
@@ -4,13 +4,21 @@
         <div class="text-center mb-5">Lorem ipsum dolor sit amet consectetur. Molestie pulvinar feugiat risus convallis et leo neque</div>
         <div class="who_we_protect__inner">
             <div class="contents">
-                <img class="content active" id="all" src="<?=get_stylesheet_directory_uri()?>/img/2024/all-employees.jpg">
+                <img class="content active" id="lone" src="<?=get_stylesheet_directory_uri()?>/img/2024/lone.jpg">
+                <img class="content" id="all" src="<?=get_stylesheet_directory_uri()?>/img/2024/all-employees.jpg">
                 <img class="content" id="home" src="<?=get_stylesheet_directory_uri()?>/img/2024/home-hybrid.jpg">
-                <img class="content" id="lone" src="<?=get_stylesheet_directory_uri()?>/img/2024/lone.jpg">
             </div>
             <div class="pills">
 
-                <div class="pill active" aria-controls="all">
+                <div class="pill active" aria-controls="lone">
+                    <h3 class="pill__title">Lone Workers</h3>
+                    <div class="pill__content">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod sed facere corporis. Eos nihil et eius esse assumenda voluptas, maxime id quibusdam perferendis expedita a?<p>
+                        <a href="#" class="button button-outline--white">Read More</a>
+                    </div>
+                </div>
+
+                <div class="pill" aria-controls="all">
                     <h3 class="pill__title">All Employees</h3>
                     <div class="pill__content">
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod sed facere corporis. Eos nihil et eius esse assumenda voluptas, maxime id quibusdam perferendis expedita a?<p>
@@ -26,13 +34,6 @@
                     </div>
                 </div>
 
-                <div class="pill" aria-controls="lone">
-                    <h3 class="pill__title">Lone Workers</h3>
-                    <div class="pill__content">
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod sed facere corporis. Eos nihil et eius esse assumenda voluptas, maxime id quibusdam perferendis expedita a?<p>
-                        <a href="#" class="button button-outline--white">Read More</a>
-                    </div>
-                </div>
             
             </div>
 
