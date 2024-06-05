30cffe32c765cf7683bc84288f20c6748ce5432c
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-06-05 13:50:33 +0100

    Update cb_who_we_protect.php

--- page-templates/blocks_2024/cb_who_we_protect.php
+++ page-templates/blocks_2024/cb_who_we_protect.php
@@ -1,7 +1,12 @@
+<?php
+$loneLink = get_field('lone_workers_link');
+$allLink = get_field('all_employees_link');
+$hybridLink = get_field('hybrid_workers_link');
+?>
 <section class="who_we_protect py-5">
     <div class="container-xl">
-        <h2 class="h1 text-center">Protecting employees for 25 years</h2>
-        <div class="text-center mb-5">Lorem ipsum dolor sit amet consectetur. Molestie pulvinar feugiat risus convallis et leo neque</div>
+        <h2 class="h1 text-center"><?=get_field('title')?></h2>
+        <div class="text-center mb-5"><?=get_field('intro')?></div>
         <div class="who_we_protect__inner">
             <div class="contents">
                 <img class="content active" id="lone" src="<?=get_stylesheet_directory_uri()?>/img/2024/lone.jpg">
@@ -13,24 +18,24 @@
                 <div class="pill active" aria-controls="lone">
                     <h3 class="pill__title">Lone Workers</h3>
                     <div class="pill__content">
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod sed facere corporis. Eos nihil et eius esse assumenda voluptas, maxime id quibusdam perferendis expedita a?<p>
-                        <a href="#" class="button button-outline--white">Read More</a>
+                        <p><?=get_field('lone_workers_content')?><p>
+                        <a href="<?=$loneLink['url']?>" class="button button-outline--white"><?=$loneLink['title']?></a>
                     </div>
                 </div>
 
                 <div class="pill" aria-controls="all">
                     <h3 class="pill__title">All Employees</h3>
                     <div class="pill__content">
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod sed facere corporis. Eos nihil et eius esse assumenda voluptas, maxime id quibusdam perferendis expedita a?<p>
-                        <a href="#" class="button button-outline--white">Read More</a>
+                        <p><?=get_field('all_employees_content')?><p>
+                        <a href="<?=$allLink['url']?>" class="button button-outline--white"><?=$allLink['title']?></a>
                     </div>
                 </div>
 
                 <div class="pill" aria-controls="home">
                     <h3 class="pill__title">Home &amp; Hybrid Workers</h3>
                     <div class="pill__content">
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod sed facere corporis. Eos nihil et eius esse assumenda voluptas, maxime id quibusdam perferendis expedita a?<p>
-                        <a href="#" class="button button-outline--white">Read More</a>
+                        <p><?=get_field('hybrid_workers_content')?><p>
+                        <a href="<?=$hybridLink['url']?>" class="button button-outline--white"><?=$hybridLink['title']?></a>
                     </div>
                 </div>
 
