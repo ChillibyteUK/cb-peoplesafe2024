aa58b57e842647d1fc04e110c6fa32734066aded
LamcatUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-07-22 15:26:54 +0100

    Update knowledge-hub.php

--- page-templates/knowledge-hub.php
+++ page-templates/knowledge-hub.php
@@ -300,7 +300,18 @@ the_post();
                 </div>
             </div>
         </section>
-
+        <section class="gradient_cta py-5 bg_grad--orange">
+            <div class="gradient_cta__image" style="--bg-url:url(<?=$img?>)"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7 offset-lg-5">
+                        <h2>Find out how you can protect your employees today</h2>
+                        <div class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sed iusto iure omnis voluptatem, in atque id hic placeat reiciendis saepe at ullam labore facere inventore dicta molestias? Quod, neque!</div>
+                        <a href="/book-a-demo-video/" class="btn">Book a Demo Today</a>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </main>
 <?php
