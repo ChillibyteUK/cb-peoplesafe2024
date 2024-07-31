47586452865b85c51bea3219c029cc27728d6b3e
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-07-31 14:23:23 +0100

    Update cb_two_cols.php

--- page-templates/blocks/cb_two_cols.php
+++ page-templates/blocks/cb_two_cols.php
@@ -1,8 +1,10 @@
 <?php
 $theme = get_field('theme');
+
+$bg = $theme != 'white' ? 'bg--' . $theme : '';
 ?>
 <!-- two_cols -->
-<section class="two_cols py-5 bg--<?=$theme?>">
+<section class="two_cols py-5 <?=$bg?>">
     <div class="container">
         <div class="row">
             <div class="col-md-6">
