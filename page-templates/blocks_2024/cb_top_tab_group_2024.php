dbf84942118658bcb1d2befdf8755463161c1e12
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-12-05 13:38:18 +0000

    Update cb_top_tab_group_2024.php

--- page-templates/blocks_2024/cb_top_tab_group_2024.php
+++ page-templates/blocks_2024/cb_top_tab_group_2024.php
@@ -5,6 +5,7 @@
             $i = 'x' . random_str(4);
             $active = 'active';
             $c = 1;
+            $d = 0;
             while (have_rows('tabs')) {
                 the_row();
 
@@ -14,11 +15,11 @@
                 $link = get_sub_field('link');
                 $image = wp_get_attachment_image(get_sub_field('image'), 'large', false, array('class' => 'content__image', 'width' => 500, 'height' => 500, 'alt' => $tab_title));
                 ?>
-            <div class="pill <?=$active?>" aria-controls="<?=$i?>_tab_<?=$c?>">
+            <div class="pill <?=$active?>" aria-controls="<?=$i?>_tab_<?=$c?>" data-aos="fade" data-aos-delay="<?=$d?>">
                 <img class="pill__icon" src="<?=$icon?>">
                 <div class="pill__title"><?=$tab_title?></div>
             </div>
-            <div class="content <?=$active?>" id="<?=$i?>_tab_<?=$c?>">
+            <div class="content <?=$active?>" id="<?=$i?>_tab_<?=$c?>" data-aos="fade">
                 <?=$image?>
                 <div class="content__inner">
                     <div class="fs-300 fw-900 text-orange"><?=$pre_title?></div>
@@ -29,6 +30,7 @@
             </div>
                 <?php
                 $c++;
+                $d+=100;
                 $active = '';
             }
             ?>
