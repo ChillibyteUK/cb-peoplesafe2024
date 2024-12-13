dab3e5d6b2e32a3ffba9ff55a0b073cc6df8f409
LamcatUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-12-13 09:33:59 +0000

    caption slider

--- page-templates/blocks_2024/cb_caption_slider_2024.php
+++ page-templates/blocks_2024/cb_caption_slider_2024.php
@@ -1,5 +1,5 @@
 <!-- logo_slider -->
-<section class="logos py-5">
+<section class="captions py-5">
     <div class="container-xl">
         <?php
         $d = 0;
@@ -9,38 +9,28 @@
                 <?= get_field('title') ?>
             </h2>
         <?php
-        } else {
-        ?>
-            <h2 class="text-center" data-aos="fade">Some of our clients</h2>
-        <?php
+            $d += 100;
         }
-        $d += 100;
         ?>
-        <div class="swiper logo__slider mb-5" data-aos="fade" data-aos-delay="<?= $d ?>">
+        <div class="swiper captions_slider mb-5" data-aos="fade" data-aos-delay="<?= $d ?>">
             <div class="swiper-wrapper">
                 <?php
-                while (have_rows('logos')) {
+                while (have_rows('cards')) {
                     the_row();
+                    $l = get_sub_field('page') ?? null;
                 ?>
-                    <div class="swiper-slide logo__slide">
-                        <?= wp_get_attachment_image(get_sub_field('logo'), 'thumbnail') ?>
+                    <div class="swiper-slide captions_slide">
+                        <a href="<?= $l['url'] ?>">
+                            <?= wp_get_attachment_image(get_sub_field('image'), 'thumbnail') ?>
+                            <?= get_sub_field('caption') ?>
+                        </a>
                     </div>
                 <?php
                 }
                 ?>
             </div>
-            <div class="swiper-pagination swiper-pagination-logos"></div>
+            <div class="swiper-pagination swiper-pagination-captions"></div>
         </div>
-        <?php
-        $d += 100;
-        if (get_field('show_case_study_link')) {
-        ?>
-            <div class="text-center" data-aos="fade" data-aos-delay="<?= $d ?>">
-                <a href="/case-studies/" class="button button-yellow"><span>View our case studies</span></a>
-            </div>
-        <?php
-        }
-        ?>
     </div>
 </section>
 <?php
@@ -48,14 +38,14 @@ add_action('wp_footer', function () {
 ?>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
-            var logoSlider = new Swiper('.logo__slider', {
+            var logoSlider = new Swiper('.captions_slider', {
                 loop: true,
                 autoplay: {
                     delay: 3000,
                     disableOnInteraction: false,
                 },
                 pagination: {
-                    el: '.swiper-pagination-logos',
+                    el: '.swiper-pagination-captions',
                     dynamicBullets: true,
                 },
                 slidesPerView: 2,
