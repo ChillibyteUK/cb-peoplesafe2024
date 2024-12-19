4a2fafb7930af7ea8fa689e3c180dd579549c8f5
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-12-19 16:22:52 +0000

    Update cb_faq_block.php

--- page-templates/blocks/cb_faq_block.php
+++ page-templates/blocks/cb_faq_block.php
@@ -9,16 +9,18 @@
         $counter = 0;
         $show = 'show';
         $collapsed = '';
+        $c = 0;
         while (have_rows('faq')) {
             the_row();
             $border = $counter == 0 ? 'border-top' : '';
-            echo '<div itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question" class="py-4 border-bottom ' . $border . '">';
+            echo '<div data-aos="fade" data-aos-delay="' . $c . '" itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question" class="py-4 border-bottom ' . $border . '">';
             echo '  <div class="question ' . $collapsed . '" itemprop="name" data-bs-toggle="collapse" id="heading_' . $counter . '" data-bs-target="#collapse_' . $counter . '" aria-expanded="true" aria-controls="collapse_' . $counter . '">' . get_sub_field('question') . '</div>';
             echo '  <div class="answer collapse ' . $show . '" itemscope="" itemprop="acceptedAnswer" itemtype="https://schema.org/Answer" id="collapse_' . $counter . '" aria-labelledby="heading_' . $counter . '" data-bs-parent="#accordion"><div class="pt-2" itemprop="text">' . apply_filters('the_content', get_sub_field('answer')) . '</div></div>';
             echo '</div>';
             $counter++;
             $show = '';
             $collapsed = 'collapsed';
+            $c += 100;
         }
         echo '</div>';
         ?>
