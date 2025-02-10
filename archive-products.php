81b999c933cdb328fa419898bd38745c0b0cbaab
Matthew Pont <matthew.pont@gmail.com>
2025-02-10 14:19:26 +0000

    Update archive-products.php

--- archive-products.php
+++ archive-products.php
@@ -9,6 +9,67 @@
 defined('ABSPATH') || exit;
 
 get_header();
+
+$user = wp_get_current_user();
+
+if($user && isset($user->user_login) && 'chillibyte' == $user->user_login) {
+?>
+<main id="main" class="pt-5">
+    <div class="container py-3">
+        <div class="page-meta text-center">
+            <h1>Safety Products</h1>
+        </div>
+        <div class="pb-4 text-center">
+            <?=apply_filters('the_content',get_field('products_intro','options'))?>
+        </div>
+        <div class="button-group filter-button-group mb-4 text-center">
+            <button class="btn btn-secondary btn-secondary--sm active mb-1" data-filter="*">All</button>
+            <?php
+            $terms = get_terms( 'ptypes', array('orderby' => 'menu_order', 'order' => 'desc') );
+            foreach ($terms as $t) {
+                echo '  <button class="btn btn-secondary btn-secondary--sm mb-1" data-filter=".' . $t->slug . '">' . $t->name . '</button>';
+            }
+            ?>
+        </div>
+        <div id="products" class="row">
+            <?php
+            while (have_posts()) {
+                the_post();
+                $types = get_the_terms(get_the_ID(),'ptypes');
+                // $type = $types ? $types[0]->slug : '';
+                $type = $types ? implode(" ", wp_list_pluck($types,'slug')) : '';
+
+                $img = get_the_post_thumbnail_url( get_the_ID(), 'large' );
+                if (get_field('alternative_product_image')) {
+                    $img = wp_get_attachment_image_url(get_field('alternative_product_image'), 'large');
+                }
+                elseif (!$img) {
+                    $img = catch_that_image($post);
+                }
+                // $cats = get_the_category();
+                // $category = $cats[0]->name;
+                ?>
+                <div class="col-md-4 mb-4 sproduct <?=$type?>">
+                    <div class="products__single">
+                        <div class="products__desc">
+                            <?=get_field( 'hover_content', get_the_ID() )?>
+                        </div>
+                        <a href="<?=get_the_permalink()?>">
+                            <div class="products__image"><img src="<?=$img?>" class="img-fluid"></div>
+                            <div class="products__title"><?=get_the_title()?></div>
+                        </a>
+                    </div>
+                </div>
+                <?php
+            }
+            ?>
+        </div>
+        <?=numeric_posts_nav()?>
+    </div>
+</section>
+</main>
+<?php
+} else {
 ?>
 <main id="main" class="pt-5">
     <div class="container py-3">
@@ -21,7 +82,7 @@ get_header();
             ?>
         </div>
         <div class="pb-4">
-			<?=apply_filters('the_content',get_field('products_intro','options'))?>
+            <?=apply_filters('the_content',get_field('products_intro','options'))?>
         </div>
         <div class="button-group filter-button-group mb-4">
             <button class="btn btn-secondary btn-secondary--sm active mb-1" data-filter="*">All</button>
@@ -69,6 +130,9 @@ get_header();
     </div>
 </section>
 </main>
+<?php
+}
+?>
 <?php
 add_action('wp_footer',function(){
     ?>
